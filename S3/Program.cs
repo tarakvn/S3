d8d87508f165c7d2c2b4882d0f6426c7faf67f9d
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:18:48 +0430

    Add project files.

---
+++ S3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int Fn = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int Sn = int.Parse(Console.ReadLine());
+            int sum = Fn + Sn;
+            Console.WriteLine("Addition of nimbers: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
